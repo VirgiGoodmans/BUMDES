530c84cff01cc634efac07c0ea75193a5106d057
Virgi Shaw <virgishaw@gmail.com>
2024-10-20 22:57:38 +0700

    New file

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\Http\Controllers;
+
+use App\Models\News;
+
+class NewsController extends Controller
+{
+    public function index()
+    {
+        // Ambil semua data dari tabel 'news'
+        $news = News::all();
+
+        // Kirim data 'news' ke view 'news.blade.php'
+        return view('news', compact('news'));
+    }
+}
