8a8a513bad96ebf48ef209bec53904e8e6eb5ae5
Virgi Shaw <virgishaw@gmail.com>
2024-10-22 23:37:40 +0700

    update fift

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,28 @@
 
 @section('content')
     <!-- Sliding News -->
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></button>
+    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('images/banner1.jpg') }}" class="d-block w-100" alt="Banner 1">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('images/banner2.jpg') }}" class="d-block w-100" alt="Banner 2">
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('images/banner3.jpg') }}" class="d-block w-100" alt="Banner 3">
+            </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="{{ asset('images/news1.jpg') }}" class="d-block w-100" alt="News 1">
